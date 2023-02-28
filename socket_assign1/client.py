4059a816408b4ffd3a27f6e26b3dc2b2f29cd9f2
garrettjns <124214129+garrettjns@users.noreply.github.com>
2023-02-28 10:43:48 -0700

    Add files via upload

--- socket_assign1/client.py
+++ socket_assign1/client.py
@@ -1,1 +1,19 @@
+from socket import *
+
+client_sock = socket(AF_INET, SOCK_STREAM)
+try:
+        client_sock.connect(("localhost", 7069))
+        while True:
+            server_data = client_sock.recv(16384)
+            if server_data:
+                print(server_data)
+            else:
+                client_sock.close()
+                break
+                   
+#cannot connect to server: close client socket
+except:
+    print("Error connecting to server")
+    client_sock.close()
+        
 
