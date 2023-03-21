97e1a50340d209eef7e06ebb5694532b4b9e7bf9
garrettjns <garrettdotjones@gmail.com>
2023-03-21 10:09:39 -0700

    added multi-threaded server

---
+++ socket_assign5/chatbot_client.py
@@ -0,0 +1,53 @@
+from socket import *
+from datetime import datetime
+
+client_sock = socket(AF_INET, SOCK_STREAM)
+
+try:
+        client_sock.connect(("localhost", 8080))
+#cannot connect to server: close client socket
+except:
+        print("Error connecting to server")
+        client_sock.close()
+
+
+client_sock.setblocking(0)
+
+counter = 0
+
+#initialize t1
+t1 = datetime.now().strftime('%H:%M:%S')
+while True:
+        try:
+                server_data = client_sock.recv(4096)
+                if server_data:
+                        print("\nserver localhost replied at " + datetime.now().strftime('%H:%M:%S') +  ": " + \
+                              server_data.decode() + "\n")
+                        if "bye" not in server_data.decode():
+                                try:
+                                        print("sending...")
+                                        client_sock.send(input().encode())
+                                        t1 = datetime.now().strftime('%H:%M:%S')
+                                        print("sent to server at : ", t1)
+                                        counter = 0
+                                except error:
+                                        continue
+                        else:
+                                client_sock.close()
+                                break
+        #the socket has no data yet
+        except error:
+                t2 = datetime.now().strftime('%H:%M:%S')
+
+                sec2 = int(t2.split(":")[2])
+                sec1 = int(t1.split(":")[2])
+
+                
+                elapsed = sec2 - sec1
+                
+                if elapsed > counter:
+                        print("at ", t2, " waiting for response, "\
+                              + str(elapsed) + " seconds have elapsed")
+                        counter += 1
+                continue
+                        
--- socket_assign5/chatbot_server.py
+++ socket_assign5/chatbot_server.py
@@ -1,1 +1,125 @@
+import argparse
+from socket import *
+import time
+import random
+import threading
 
+parser = argparse.ArgumentParser(description="a chatbot program")
+
+# responses
+
+gender = {"female" : "\nHow excellent! Are you a CS major?\n", "male" : "\nMe too. Are you a CS Major?\n"}
+
+cs_major = {"no" : "\nToo bad. Anyway, what's an animal you don't like, and two you do?\n",
+            "yes" : "\nExcellent, I am too. What's an animal you don't like, and two you do?\n"}
+
+def sorry(inp):
+    return "\nSorry, I don't understand " + "\'" + inp + "\'"
+
+def response1(inp):
+    if "female" in inp.lower() or "woman" in inp.lower():
+        return(gender["female"])
+    elif "male" in inp.lower() or "man" in inp.lower():
+        return(gender["male"])
+    elif "bye" in inp.lower():
+        return 0
+    else:
+        return(sorry(inp) + "\nAnyway, are you a CS major?\n")
+
+def response2(inp):
+    if "no" in inp.lower() or "nah" in inp.lower():
+        return(cs_major["no"])
+    elif "yes" in inp.lower() or "yea" in inp.lower():
+        return(cs_major["yes"])
+    elif "bye" in inp.lower():
+        return 0
+    else:
+        return(sorry(inp) + "\nAnyway, what's an animal you don't like, and two you do?\n")
+
+def response3(inp):
+    if "," in inp:
+        inp = inp.split(",")
+    elif "and" in inp:
+        inp = inp.split("and")
+    elif "bye" in inp.lower():
+        return 0
+    else:
+        inp = inp.split(" ")
+    if len(inp) < 2:
+        return 0
+    return("\n\n" + inp[1] + " are awesome, but I hate " + inp[0] + " too.")
+
+socket_states = {}
+
+def socket_handler():
+    # The server is listening at 8080 
+    tcpSerSock = socket(AF_INET, SOCK_STREAM)
+    tcpSerSock.setblocking(0)
+    tcpSerSock.bind(("localhost", 8080))
+    tcpSerSock.listen(5)
+    return tcpSerSock
+
+def delete_socket(s):
+    s.send(b'Goodbye for now!')
+    del socket_states[s]
+    s.close()
+
+# Introduces delay before send
+def time_func():
+    print(time.sleep(int(4*random.random())))
+
+def thread_function(client_sock, thread_num):
+    print("Thread %d: starting" % (thread_num))
+    client_sock.send(b'\nHello, are you male or female?\n')    
+    while True:
+        try:
+            data = client_sock.recv(4096)
+        except:
+            continue
+        if data:
+            print(client_sock.getpeername(), ": ", socket_states[client_sock])
+            # A readable client socket has data
+            if socket_states[client_sock] == 1:
+                response = response1(data.decode())
+                if response == 0:
+                    delete_socket(client_sock)
+                    break
+                time_func()
+                client_sock.send(response.encode())
+                socket_states[client_sock] = 2
+                
+            elif socket_states[client_sock] == 2:
+                response = response2(data.decode())
+                if response == 0:
+                    delete_socket(client_sock)
+                    break
+                time_func()
+                client_sock.send(response.encode())
+                socket_states[client_sock] = 3
+            
+            elif socket_states[client_sock] == 3:
+                response = response3(data.decode())
+                if response == 0:
+                    delete_socket(client_sock)
+                    break
+                time_func()
+                client_sock.send(response.encode() + b'\nGoodbye for now!')
+                del socket_states[client_sock]               
+                client_sock.close()
+                break
+    print("Thread %d: finishing" % (thread_num))
+
+server_sock = socket_handler()
+thread_num = 0
+
+while True:
+    try:
+        client_sock, addr = server_sock.accept()
+    except:
+        continue
+    client_sock.setblocking(0)
+    socket_states[client_sock] = 1
+    thread_num += 1
+    print("Main    : create and start thread %d." % (thread_num))
+    new_thread = threading.Thread(target=thread_function, args=(client_sock, thread_num,))
+    new_thread.start()
